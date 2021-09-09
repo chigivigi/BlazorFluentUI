4c6b6f35f61eda24288ef95d23436a1a7571a86f
Jure Fadiga <jfadiga@3pro.eu>
2021-09-09 09:21:32 +0200

    example of sort problem

---
+++ Demo/BlazorFluentUI.Demo.Shared/Models/CustomGrid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFluentUI.Demo.Shared.Models
+{
+    public class CustomGrid
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string lastname { get; set; }
+        public string type { get; set; }
+        public int amount { get; set; }
+        public Guid secondid { get; set; }
+        public bool visible { get; set; }
+        public string status { get; set; }
+        public IDropdownOption secondstatus { get; set; }
+    }
+}
